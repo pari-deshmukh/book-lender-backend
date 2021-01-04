3336680290fd141e3e5748e18930d4a73270524c
Pranali Deshmukh <deshmuk3@coventry.ac.uk>
2021-01-04 18:31:14 +0530

    feat(sql): adds scripts to populate db tables
    adds sql scripts to populate users, books, genres and bookGenres tables
    
    ref: #13

---
+++ sql_scripts/9. populate_users.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (role, firstName, lastName, username, postalAddress, password, email) VALUES ('user', 'John', 'Doe', 'johndoe', 'some random address', 'xtkmJByErs9bWDB05UvhbuNuvld4EtOZHiiYi.WPIu80Ap0pC0mvy', 'john@doe.com');
+INSERT INTO users (role, firstName, lastName, username, postalAddress, password, email) VALUES ('admin', 'Jane', 'Doe', 'janedoe', 'some random address', 'xtkmJByErs9bWDB05UvhbuNuvld4EtOZHiiYi.WPIu80Ap0pC0mvy', 'jane@doe.com');
